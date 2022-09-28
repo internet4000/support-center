01691fc8f1bb6290f37287bbd67b97e1bb4ef374
user-09876 <user-09876@users.noreply.github.com>
2022-09-28 14:49:03 +0000

    deploy: ba574b3a4fd2bec539fe255edf63df01ae92a31f

---
+++ docs.js
@@ -0,0 +1,19 @@
+import 'https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js'
+
+function addAnchor(element) {
+	element.innerHTML = `<a href="#${element.id}">${element.innerText}</a>`
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+	docsearch({
+		apiKey: '89255c22080c15165cab8c9186becc6b',
+		indexName: 'internet_4000',
+		inputSelector: '#search',
+		debug: true
+	});
+
+	var headers = document.querySelectorAll('article h2, article h3')
+	if (headers) {
+		headers.forEach(addAnchor)
+	}
+});
